ba7ea47f80f396132005649941a1e9d220d95b4a
Rohit <katariyarohit16@gmail.com>
2021-09-01 15:56:07 +0530

    theme extraction

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
 import 'package:flutter_application_1/utills/routes.dart';
+import 'package:flutter_application_1/widgets/thems.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,12 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyThems.lightThems(context),
+      darkTheme: MyThems.darkThems(context),
       initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => LoginPage(),
